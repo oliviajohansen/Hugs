0e3e569d6dbc716e2108b0134d5c2d8107dbe4cb
successs404 <neoruien@u.nus.edu>
2020-07-02 16:57:46 +0800

    'updates'

---
+++ lib/pages/authenticate/login.dart
@@ -0,0 +1,311 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:hugsmobileapp/services/authentication.dart';
+import '../bottomNavBar.dart';
+
+class Login extends StatefulWidget {
+
+  Function toggleView;
+  Login({this.toggleView});
+
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+
+  final Authentication _auth = Authentication();
+  final _formKey = GlobalKey<FormState>();
+  String error = '';
+
+  String email = '';
+  String password = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        child: Stack(
+          children: <Widget> [
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    stops: [
+                      0.6,
+                      0.4
+                    ],
+                    colors: [Color(0XffFFE289), Color(0XFFE289)]
+                ),
+              ),
+            ),
+            Container(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 55.0),
+                  child: Align(
+                      alignment: Alignment.topCenter,
+                      child: Image.asset('assets/images/Hugs logo.png',
+                          height: 80.0,
+                          width: 80.0)
+                  ),
+                )
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(40.0, 0.0, 40.0, 25.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget> [
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(5),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.4),
+                            blurRadius: 10.0,
+                          )
+                        ]
+                    ),
+                    child: Column(
+                      children: <Widget> [
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(10, 8, 10, 10),
+                          child: Text('Welcome to Hugs!',
+                            style: TextStyle(
+                                fontFamily: 'Poppins',
+                                fontSize: 28.0,
+                                fontWeight: FontWeight.bold
+                            ),
+                          ),
+                        ),
+                        Form(
+                          key: _formKey,
+                          child: Column(
+                            children: <Widget> [
+                              Container(
+                                  padding: EdgeInsets.fromLTRB(25.0, 3.0, 0.0, 5.0),
+                                  decoration: BoxDecoration(
+                                      border: Border(bottom: BorderSide(color: Colors.grey[100]))
+                                  ),
+                                  child: TextFormField(
+                                      decoration: InputDecoration(
+                                          border: InputBorder.none,
+                                          hintText: "Username or Email",
+                                          hintStyle: TextStyle(color: Colors.grey[400])
+                                      ),
+                                    onChanged: (val) {
+                                      setState(() => email = val);
+                                    },
+                                    validator: (val) => val.isEmpty
+                                        ? 'Please enter an email'
+                                        : null,
+                                  )
+                              ),
+                              Container(
+                                  padding: EdgeInsets.fromLTRB(25.0, 5.0, 0.0, 3.0),
+                                  child: TextFormField(
+                                    decoration: InputDecoration(
+                                        border: InputBorder.none,
+                                        hintText: "Password",
+                                        hintStyle: TextStyle(color: Colors.grey[400])
+                                    ),
+                                    obscureText: true,
+                                    onChanged: (val) {
+                                      setState(() => password = val);
+                                    },
+                                    validator: (val) => val.length  < 6
+                                        ? 'Please enter minimum 6 characters'
+                                        : null,
+                                  )
+                              ),
+                           ]
+                          )
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(0.0, 0.0, 20.0, 10.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: <Widget> [
+                              new GestureDetector(
+                                onTap: () {print('I forgot password');},
+                                child: Text('Forgot Password?',
+                                    style: TextStyle(
+                                        color: Color(0XffFFE289),
+                                        fontFamily: 'Poppins',
+                                        fontWeight: FontWeight.bold,
+                                        shadows: <Shadow> [
+                                          Shadow(
+                                            offset: Offset(1.3, 1.3),
+                                            color: Colors.grey.withOpacity(0.8),
+                                          )
+                                        ]
+                                    )
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(height: 10.0),
+                        ButtonTheme(
+                          minWidth: 200,
+                          height: 40,
+                          child: RaisedButton(
+                            child: Text('Login',
+                                style: TextStyle(
+                                    fontFamily: 'Poppins',
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20.0,
+                                    shadows: <Shadow> [
+                                      Shadow(
+                                        offset: Offset(2.5, 2.5),
+                                        color: Colors.grey.withOpacity(0.6),
+                                      )
+                                    ]
+                                )),
+                            color: Color(0XffFFE289),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            onPressed: () async {
+                              if (_formKey.currentState.validate()) {
+                                dynamic result = await _auth
+                                    .signInWithEmailAndPassword(
+                                    email, password);
+                                if (result == null) {
+                                  setState(() {
+                                    error = 'Could not sign in with those credentials';
+                                  });
+                                  print(error);
+                                } else {
+                                  print('successful login');
+                                }
+                              }
+                            }
+                          ),
+                        ),
+                        SizedBox(height: 10.0)
+                      ],
+                    ),
+                  ),
+                  Text(error, style: TextStyle(color: Colors.red, fontSize: 14.0)),
+                  SizedBox(height: 30.0),
+                  Row(
+                      children: <Widget> [
+                        Expanded(
+                          child: Divider(
+                            color: Colors.grey.withOpacity(0.9),
+                            height: 20.0,
+                            thickness: 0.3,
+                            endIndent: 15.0,
+                          ),
+                        ),
+                        Text('Login with',
+                            style: TextStyle(
+                              fontFamily: 'Poppins',
+                              color: Colors.grey.withOpacity(0.6),
+                              fontWeight: FontWeight.bold,
+                              //fontSize: 20.0,
+                            )
+                        ),
+                        Expanded(
+                          child: Divider(
+                            color: Colors.grey.withOpacity(0.9),
+                            height: 20.0,
+                            thickness: 0.3,
+                            indent: 15.0,
+                          ),
+                        ),
+                      ]
+                  ),
+                  SizedBox(height: 10.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget> [
+                      Material(
+                        shape: CircleBorder(),
+                        clipBehavior: Clip.hardEdge,
+                        child: Ink.image(
+                            image: AssetImage('assets/images/Facebook.png'),
+                            fit: BoxFit.cover,
+                            width: 35.0,
+                            height: 35.0,
+                            child: InkWell(
+                              onTap: () {
+                                _auth.signInWithFacebook();
+                              },
+                            )
+                        ),
+                      ),
+                      SizedBox(width: 20.0),
+                      Material(
+                        shape: CircleBorder(),
+                        clipBehavior: Clip.hardEdge,
+                        color: Colors.transparent,
+                        child: Ink.image(
+                            image: AssetImage('assets/images/Google.png'),
+                            fit: BoxFit.cover,
+                            width: 35.0,
+                            height: 35.0,
+                            child: InkWell(
+                              onTap: () {
+                                _auth.signInWithGoogle();
+                              },
+                            )
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20.0),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget> [
+                        GestureDetector(
+                          onTap: () {print('new user tapped');},
+                          child: Text('New user? ',
+                              style: TextStyle(
+                                  fontFamily: 'Poppins',
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                  shadows: <Shadow> [
+                                    Shadow(
+                                      offset: Offset(1.0, 1.0),
+                                      color: Colors.grey.withOpacity(0.8),
+                                    )
+                                  ]
+                              )
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            widget.toggleView();
+                          },
+                          child: Text('Sign up',
+                              style: TextStyle(
+                                  fontFamily: 'Poppins',
+                                  color: Color(0XffFFE289),
+                                  fontWeight: FontWeight.bold,
+                                  shadows: <Shadow> [
+                                    Shadow(
+                                      offset: Offset(1.3, 1.3),
+                                      color: Colors.grey.withOpacity(0.8),
+                                    )
+                                  ]
+                              )
+                          ),
+                        )
+                      ]
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavBar(),
+    );
+  }
+}
+
---
+++ lib/pages/other_profile.dart
@@ -0,0 +1,336 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_snake_navigationbar/flutter_snake_navigationbar.dart';
+
+class OtherProfile extends StatefulWidget {
+  @override
+  _OtherProfileState createState() => _OtherProfileState();
+}
+
+class _OtherProfileState extends State<OtherProfile> {
+
+  int position = 4;
+
+  bool isHugsDelivered = true;
+
+  List<String> imgsHugsDelivered = [
+    'images/A.jpg',
+    'images/B.jpg',
+    'images/C.jpg',
+    'images/D.jpg',
+    'images/Heart.png',
+    'images/Hug 1.png',
+    'images/Hug 2.png',
+    'images/Profile pic.png',
+    'images/Settings icon.png'
+  ];
+
+  List<String> imgsHugsReceived = [
+    'images/Hug 1.png',
+    'images/Hug 2.png'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xfffafafa),
+      body: SafeArea(
+          child: Stack(
+            children: <Widget>[
+              Align(
+                alignment: Alignment.topRight,
+                child: Container(
+                    padding: EdgeInsets.all(30.0),
+                    child: IconButton(
+                        onPressed: () {},
+                        icon: Image.asset(
+                            'images/Settings icon.png',
+                            scale: 15)
+                    )
+                ),
+              ),
+              Column(
+                children: <Widget>[
+                  SizedBox(height: 28.0),
+                  Row(
+                    children: <Widget>[
+                      SizedBox(width: 28.0),
+                      Column(
+                          children: <Widget> [
+                            CircleAvatar(
+                              backgroundImage: AssetImage('images/Profile picture (Mocha).png'),
+                              backgroundColor: Color(0xffE8E7E7),
+                              radius: 121/2,
+                            ),
+                            SizedBox(height: 5),
+                            Container(
+                                width: 90,
+                                height: 24.0,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(17.0),
+                                  color: Color(0xffffc000),
+                                ),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: <Widget>[
+                                    SizedBox(width: 5.0),
+                                    Container(
+                                      padding: EdgeInsets.all(5),
+                                      decoration: BoxDecoration(
+                                          color: Colors.white,
+                                          shape: BoxShape.circle
+                                      ),
+                                      child: Text(
+                                        '8',
+                                        style: TextStyle(
+                                          fontFamily: 'Poppins',
+                                          fontSize: 10,
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                      ),
+                                    ),
+                                    SizedBox(width: 15.0),
+                                    Text(
+                                      '14/50',
+                                      style: TextStyle(
+                                        fontFamily: 'Poppins',
+                                        fontSize: 12,
+                                        color: const Color(0xffffffff),
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                      textAlign: TextAlign.center,
+                                    )
+                                  ],
+                                )
+                            ),
+                          ]
+                      ),
+                      SizedBox(width: 168.5-149.5),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'Emma Lim',
+                            style: TextStyle(
+                              fontSize: 21.0,
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                          Text(
+                            'Gracious Giver',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.w500,
+                              color: Color(0xff7A8FA6),
+                            ),
+                          ),
+                          Text(
+                            'Hello, nice to meet you!',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.w400,
+                              color: Color(0xff7A8FA6),
+                            ),
+                            maxLines: null,
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                  SizedBox(height: 15.0),
+                  Divider(
+                    height: 36,
+                    color: Colors.grey[850],
+                  ),
+                  SizedBox(height: 46),
+                  FlatButton(
+                    onPressed: () {},
+                    child: Container(
+                      width: 302.0,
+                      height: 71.0,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Color(0xff59B3CA),
+                          width: 3
+                        ),
+                        borderRadius: BorderRadius.circular(15.0),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.white,
+                              offset: Offset(0, 3),
+                              blurRadius: 36)
+                        ],
+                      ),
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Text(
+                            'Send Emma Lim a hug',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.w400,
+                            color: Color(0xff7A8FA6),
+                          ),
+                        ),
+                      ),
+                    )
+                  ),
+                  SizedBox(height: 25),
+                  FlatButton(
+                    onPressed: () {},
+                      child: Container(
+                        width: 302.0,
+                        height: 71.0,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                              color: Color(0xff59B3CA),
+                              width: 3
+                          ),
+                          borderRadius: BorderRadius.circular(15.0),
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.white,
+                                offset: Offset(0, 3),
+                                blurRadius: 36)
+                          ],
+                        ),
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: Text(
+                            'Send Emma Lim a message',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.w400,
+                              color: Color(0xff7A8FA6),
+                            ),
+                          ),
+                        ),
+                      )
+                  )
+                ],
+              )
+            ],
+          )
+      ),
+      bottomNavigationBar: SnakeNavigationBar(
+        style: SnakeBarStyle.pinned,
+        backgroundColor: Color(0x94FAFAFA),
+        currentIndex: position,
+        onPositionChanged: (index) => setState(() => position = index),
+        items: [
+          BottomNavigationBarItem(
+            icon: Image.asset('images/Home button.png'),
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset('images/Chat button.png'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset('images/Trophy button.png'),
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset('images/Profile button.png'),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+/*
+                Container(
+                  width: 302.0,
+                  height: 71.0,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15.0),
+                    color: const Color(0xffffffff),
+                    boxShadow: [
+                      BoxShadow(
+                          color: const Color(0x08000000),
+                          offset: Offset(0, 3),
+                          blurRadius: 36)
+                    ],
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Container(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: <Widget>[
+                            Text(
+                              '109',
+                              style: TextStyle(
+                                fontFamily: 'Poppins',
+                                fontSize: 20,
+                                color: const Color(0xff1b1b1b),
+                                fontWeight: FontWeight.w700,
+                              )
+                            ),
+                            Text(
+                              'Hugs Sent',
+                              style: TextStyle(
+                                fontFamily: 'Poppins',
+                                fontSize: 13,
+                                color: const Color(0x997a8fa6),
+                              )
+                            ),
+                            SizedBox(height: 12.0)
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: <Widget>[
+                            Image.asset('images/Heart.png'),
+                            Text(
+                                'Experience',
+                                style: TextStyle(
+                                  fontFamily: 'Poppins',
+                                  fontSize: 13,
+                                  color: const Color(0x997a8fa6),
+                                )
+                            ),
+                            SizedBox(height: 12.0)
+                          ],
+                        )
+                      ),
+                      Container(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: <Widget>[
+                            Text(
+                              '71',
+                              style: TextStyle(
+                                fontFamily: 'Poppins',
+                                fontSize: 20,
+                                color: const Color(0xff1b1b1b),
+                                fontWeight: FontWeight.w700,
+                              )
+                            ),
+                            Text(
+                              'Following',
+                              style: TextStyle(
+                                fontFamily: 'Poppins',
+                                fontSize: 13,
+                                color: const Color(0x997a8fa6),
+                              )
+                            ),
+                            SizedBox(height: 12.0)
+                          ],
+                        ),
+                      ),
+                    ],
+                  )
+                ),
+ */
